488da5f0a6127e6eeb07f36ad78e10690ffc0866
ShaneH <shane@mbp.local>
2014-05-05 16:45:36 +0800

    time

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -4,4 +4,11 @@ var args = require('minimist')(process.argv.slice(2));
 var app = createMiniHarp();
 var port = args.port || 4000;
 console.log('Starting mini-harp on http://localhost:' + port);
+app.use(function(req, res, next) {
+    if(req.url == '/current-time') {
+        res.end(new Date().toISOString() + '\n');
+    } else {
+        next();
+    }
+});
 app.listen(port);
--- empty.js
+++ empty.js
@@ -1,6 +1,8 @@
 var connect = require('connect');
 
 var app = connect();
-
+for(var name in connect) {
+    console.log(name, connect[name]);
+}
 console.log('Starting http server on http://localhost:4000');
 app.listen(4000);
