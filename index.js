479280524cb1c6d28d502b7afdb181dd38b7c76d
ShaneH <shane@mbp.local>
2014-05-06 20:13:39 +0800

    jade

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var serveStatic = require('serve-static');
 var connect = require('connect');
+var makeJade = require('./lib/processor/jade.js');
 module.exports = function(root) {
-    return connect().use(serveStatic(root));
+    var jade = makeJade(root);
+    //return connect().use(jade).use(serveStatic(root));
+    return connect().use(serveStatic(root)).use(jade);
 };
