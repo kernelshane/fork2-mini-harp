778a88182964cc91ba8180c4b25644c3e81de88a
ShaneH <shane@mbp.local>
2014-05-06 20:53:35 +0800

    index url

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var connect = require('connect');
 var makeJade = require('./lib/processor/jade.js');
 module.exports = function(root) {
     var jade = makeJade(root);
-    //return connect().use(jade).use(serveStatic(root));
-    return connect().use(serveStatic(root)).use(jade);
+    return connect()
+        .use(function(req, res, next) {
+            if (req.url == "/") {
+                req.url = "/index.html";
+            }
+            next();
+        })
+        .use(serveStatic(root))
+        .use(jade)
+        
 };
